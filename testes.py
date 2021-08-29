117c4123604ad905d2c580e07b54a88bbf7e9518
RHGodoy <rhgodoy@gmail.com>
2021-08-29 18:15:13 -0300

    Início do programa de testes... Vou montando aos poucos e subindo.

--- testes.py
+++ testes.py
@@ -1,1 +1,18 @@
 import unittest
+from locadora import Locadora
+from locadora import Cliente
+
+class testes(unittest.TestCase):
+    def setUp(self):
+        self.locadora = Locadora(10, 5, 25, 100)
+        self.cliente = Cliente("Ricardo", 1)
+
+    def testaCriaNovoCliente(self):
+        pass
+
+    def testaNovoAluguel(self):
+        pass
+
+    def testaEncerraAluguelCasoIdeal(self):
+        pass
+
